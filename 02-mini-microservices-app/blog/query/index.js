9e2bce1739903779e932628920e0f54578595fa4
pcsmomo <dongwoo_333@hanmail.net>
2021-12-12 08:07:00 +1100

    37. Parsing Incoming Events

--- 02-mini-microservices-app/blog/query/index.js
+++ 02-mini-microservices-app/blog/query/index.js
@@ -6,9 +6,42 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/posts', (req, res) => {});
+const posts = {};
+// QUICK EXAMPLE
+// POSTS === {
+//   '123asdf': {
+//     id: '123asdf',
+//     title: 'post title',
+//     comments: [
+//       { id: '9kvs8', content: 'content' }
+//     ]
+//   }
+// }
 
-app.get('/events', (req, res) => {});
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === 'CommentCreated') {
+    const { id, content, postId } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+
+  console.log(posts);
+
+  res.send({});
+});
 
 app.listen(4002, () => {
   console.log('Listening on 4002');
