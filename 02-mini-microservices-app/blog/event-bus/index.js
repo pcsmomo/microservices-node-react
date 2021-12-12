3c85febd4af2ec98f2d6726b72667f9a513780f0
pcsmomo <dongwoo_333@hanmail.net>
2021-12-12 15:34:15 +1100

    52. Implementing Event Sync

--- 02-mini-microservices-app/blog/event-bus/index.js
+++ 02-mini-microservices-app/blog/event-bus/index.js
@@ -4,8 +4,12 @@ const axios = require('axios');
 const app = express();
 app.use(express.json());
 
+const events = [];
+
 app.post('/events', (req, res) => {
   const event = req.body;
+
+  events.push(event);
 
   axios.post('http://localhost:4000/events', event).catch(err => {
     console.log(err.message);
@@ -21,6 +25,10 @@ app.post('/events', (req, res) => {
   });
 
   res.send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
--- 02-mini-microservices-app/blog/query/index.js
+++ 02-mini-microservices-app/blog/query/index.js
@@ -18,13 +18,7 @@ const posts = {};
 //   }
 // }
 
-app.get('/posts', (req, res) => {
-  res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-  const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
   if (type === 'PostCreated') {
     const { id, title } = data;
 
@@ -49,12 +43,28 @@ app.post('/events', (req, res) => {
     comment.status = status;
     comment.content = content;
   }
+};
 
-  console.log(posts);
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+  handleEvent(type, data);
 
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log('Listening on 4002');
+
+  const res = await axios.get('http://localhost:4005/events');
+
+  for (let event of res.data) {
+    console.log('Processing event:', event.type);
+
+    handleEvent(event.type, event.data);
+  }
 });
