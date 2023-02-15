60d7740b079dc04e03bc0fd612242ac090e228d2
Noah Kim <dongwoo_333@hanmail.net>
2023-02-16 07:52:00 +1100

    133. Scaffolding Routes

--- 07-response-normalization/ticketing/auth/src/index.ts
+++ 07-response-normalization/ticketing/auth/src/index.ts
@@ -1,11 +1,17 @@
 import express, { json } from 'express';
 
 import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
 
 const app = express();
 app.use(json());
 
 app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 // The port wouldn't matter when we start kubernetes
 app.listen(3000, () => {
