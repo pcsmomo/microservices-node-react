595135512d34ad2d6a5db71d638321b3a9c7fae8
Noah Kim <dongwoo_333@hanmail.net>
2023-02-17 08:18:29 +1100

    144. Determining Error Type

--- 07-response-normalization/ticketing/auth/src/middlewares/error-handler.ts
+++ 07-response-normalization/ticketing/auth/src/middlewares/error-handler.ts
@@ -1,12 +1,22 @@
 import { Request, Response, NextFunction } from 'express';
 
+// Custom error types
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
+
 export const errorHandler = (
   err: Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  console.error('Something went wrong', err);
+  if (err instanceof RequestValidationError) {
+    console.log('handling this error as a request validation error');
+  }
+
+  if (err instanceof DatabaseConnectionError) {
+    console.log('handling this error as a database connection error');
+  }
 
   res.status(400).send({
     message: err.message,
