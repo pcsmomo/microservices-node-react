254844c05f4a34a952af976f04ebc5f98c146c29
Noah Kim <dongwoo_333@hanmail.net>
2023-02-17 18:41:04 +1100

    149. How to Define New Custom Errors

--- 07-response-normalization/ticketing/auth/src/index.ts
+++ 07-response-normalization/ticketing/auth/src/index.ts
@@ -7,6 +7,7 @@ import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
+import { NotFoundError } from './errors/not-found-error';
 
 // Middlewares
 import { errorHandler } from './middlewares/error-handler';
@@ -25,6 +26,10 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.all('*', () => {
+  throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
