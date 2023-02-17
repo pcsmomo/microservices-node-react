78c36d412d3aec7e3172c455de243b127612ef35
Noah Kim <dongwoo_333@hanmail.net>
2023-02-17 16:08:01 +1100

    146. Moving Logic Into Errors

--- 07-response-normalization/ticketing/auth/src/errors/database-connection-error.ts
+++ 07-response-normalization/ticketing/auth/src/errors/database-connection-error.ts
@@ -1,9 +1,14 @@
 export class DatabaseConnectionError extends Error {
+  statusCode = 500;
   reason = 'Error connecting to database';
   constructor() {
     super();
 
     // Only because we are extending a built in class
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: this.reason }];
   }
 }
