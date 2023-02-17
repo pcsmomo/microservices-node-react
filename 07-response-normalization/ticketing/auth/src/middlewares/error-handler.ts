c30842b1c0aaa0544c11dd42e9e9884d92a96298
Noah Kim <dongwoo_333@hanmail.net>
2023-02-17 18:27:28 +1100

    148. Final Error Related Code

--- 07-response-normalization/ticketing/auth/src/middlewares/error-handler.ts
+++ 07-response-normalization/ticketing/auth/src/middlewares/error-handler.ts
@@ -1,8 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 
-// Custom error types
-import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+// Custom error type
+import { CustomError } from '../errors/custom-error';
 
 export const errorHandler = (
   err: Error,
@@ -10,11 +9,7 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  if (err instanceof RequestValidationError) {
-    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
-  }
-
-  if (err instanceof DatabaseConnectionError) {
+  if (err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
