240d2e045fb51361875d1732b7d7b4a059faefce
Noah Kim <dongwoo_333@hanmail.net>
2023-02-21 16:22:00 +1100

    241. Specifying the Host

--- 11-ssr-react-app/ticketing/client/pages/index.js
+++ 11-ssr-react-app/ticketing/client/pages/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const LandingPage = ({ currentUser }) => {
-  // console.log(currentUser);
+  console.log(currentUser);
   // axios.get('/api/users/currentuser').catch(err => {
   //   console.log(err.message);
   // });
@@ -17,13 +17,27 @@ const LandingPage = ({ currentUser }) => {
 
 // getServerSideProps can replace getIinitialProps as getIinitialProps is considered as a legacy method
 // But leave it for now to see the usecase of getIinitialProps
-LandingPage.getInitialProps = async () => {
+LandingPage.getInitialProps = async ({ req }) => {
   if (typeof window === 'undefined') {
     // we are on the server
     // requests should be made to http://ingress-nginx-controller.ingress-nginx.svc.cluster.local
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: {
+          Host: 'ticketing.dev',
+        },
+      }
+    );
+
+    return data;
   } else {
     // we are on the browser
     // requests can be made with a base url of ''
+    const { data } = await axios.get('/api/users/currentuser');
+
+    // { currentUser: null }
+    return data;
   }
   return {};
 };
