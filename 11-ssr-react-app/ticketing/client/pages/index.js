90f870ca8ddb69de120e79be254a6be48510d524
Noah Kim <dongwoo_333@hanmail.net>
2023-02-21 16:33:54 +1100

    243. A Reusable API Client

--- 11-ssr-react-app/ticketing/client/pages/index.js
+++ 11-ssr-react-app/ticketing/client/pages/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
@@ -11,34 +11,16 @@ const LandingPage = ({ currentUser }) => {
 
 // export async function getServerSideProps() {
 //   console.log('I am on the server');
-
 //   return { props: { color: 'red' } };
 // }
 
 // getServerSideProps can replace getIinitialProps as getIinitialProps is considered as a legacy method
 // But leave it for now to see the usecase of getIinitialProps
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === 'undefined') {
-    // we are on the server
-    // requests should be made to http://ingress-nginx-controller.ingress-nginx.svc.cluster.local
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        headers: req.headers,
-      }
-    );
+LandingPage.getInitialProps = async context => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
 
-    // console.log(req.headers);
-
-    return data;
-  } else {
-    // we are on the browser
-    // requests can be made with a base url of ''
-    const { data } = await axios.get('/api/users/currentuser');
-
-    // { currentUser: null }
-    return data;
-  }
+  return data;
 };
 
 export default LandingPage;
