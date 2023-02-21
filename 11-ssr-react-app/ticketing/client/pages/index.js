0107f510600ae9e00722c7b7d19070cff024b98a
Noah Kim <dongwoo_333@hanmail.net>
2023-02-21 18:16:19 +1100

    244. Content on the Landing Page

--- 11-ssr-react-app/ticketing/client/pages/index.js
+++ 11-ssr-react-app/ticketing/client/pages/index.js
@@ -1,12 +1,11 @@
 import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  // axios.get('/api/users/currentuser').catch(err => {
-  //   console.log(err.message);
-  // });
-
-  return <h1>Landing Page</h1>;
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
+  );
 };
 
 // export async function getServerSideProps() {
