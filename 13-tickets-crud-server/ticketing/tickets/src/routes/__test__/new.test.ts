bb75b14daa4898c9b3f93e28a6560d9c27db047f
Noah Kim <dongwoo_333@hanmail.net>
2023-02-24 07:54:43 +1100

    281. Creation via Route Handler

--- 13-tickets-crud-server/ticketing/tickets/src/routes/__test__/new.test.ts
+++ 13-tickets-crud-server/ticketing/tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
   const response = await request(app).post('/api/tickets').send({});
@@ -49,10 +50,19 @@ it('returns an error if an invalid price is provided', async () => {
 });
 
 it('creates a ticket with valid inputs', async () => {
-  // add in a check to make sure a ticket was saved
+  let tickets = await Ticket.find({});
+  expect(tickets).toHaveLength(0);
+
+  const title = 'test ticket';
 
   await request(app)
     .post('/api/tickets')
-    .send({ title: 'test ticket', price: 20 })
+    .set('Cookie', global.signin())
+    .send({ title, price: 20 })
     .expect(201);
+
+  tickets = await Ticket.find({});
+  expect(tickets).toHaveLength(1);
+  expect(tickets[0].price).toEqual(20);
+  expect(tickets[0].title).toEqual(title);
 });
--- 13-tickets-crud-server/ticketing/tickets/src/routes/new.ts
+++ 13-tickets-crud-server/ticketing/tickets/src/routes/new.ts
@@ -4,6 +4,9 @@ import { body } from 'express-validator';
 // Middleware
 import { requireAuth, validateRequest } from '@dwktickets/common';
 
+// Models
+import { Ticket } from '../models/ticket';
+
 const router = express.Router();
 
 router.post(
@@ -16,8 +19,18 @@ router.post(
       .withMessage('Price must be greater than 0'),
   ],
   validateRequest,
-  (req: Request, res: Response) => {
-    res.sendStatus(200);
+  async (req: Request, res: Response) => {
+    const { title, price } = req.body;
+
+    const ticket = Ticket.build({
+      title,
+      price,
+      userId: req.currentUser!.id, // it will be checked in requireAuth
+      // we are confident so we can use ! to tell TS that we are sure that currentUser is defined
+    });
+    await ticket.save();
+
+    res.status(201).send(ticket);
   }
 );
 
