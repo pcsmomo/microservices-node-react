a229a22e2a2813989d11dfa2611442058d2faca1
Noah Kim <dongwoo_333@hanmail.net>
2023-02-24 07:39:24 +1100

    279. Reminder on Mongoose with TypeScript

--- 13-tickets-crud-server/ticketing/tickets/src/routes/__test__/new.test.ts
+++ 13-tickets-crud-server/ticketing/tickets/src/routes/__test__/new.test.ts
@@ -48,4 +48,11 @@ it('returns an error if an invalid price is provided', async () => {
     .expect(400);
 });
 
-it('creates a ticket with valid inputs', async () => {});
+it('creates a ticket with valid inputs', async () => {
+  // add in a check to make sure a ticket was saved
+
+  await request(app)
+    .post('/api/tickets')
+    .send({ title: 'test ticket', price: 20 })
+    .expect(201);
+});
