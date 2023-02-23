5430900df0f1d27b2303a4e9a167f58626a03877
Noah Kim <dongwoo_333@hanmail.net>
2023-02-23 16:19:05 +1100

    270. Quick Auth Update

--- 13-tickets-crud-server/ticketing/auth/src/index.ts
+++ 13-tickets-crud-server/ticketing/auth/src/index.ts
@@ -7,10 +7,13 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
 
   // Connect to the database
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    await mongoose.connect(process.env.MONGO_URI);
     console.info('Connected to MongoDb');
   } catch (err) {
     console.error(err);
