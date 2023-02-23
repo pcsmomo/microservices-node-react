2529d5f4d8a3d3ae14c347ec0776d2b71a9b32b3
Noah Kim <dongwoo_333@hanmail.net>
2023-02-24 07:10:00 +1100

    276. Building a Session

--- 13-tickets-crud-server/ticketing/tickets/src/test/setup.ts
+++ 13-tickets-crud-server/ticketing/tickets/src/test/setup.ts
@@ -1,12 +1,13 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import request from 'supertest';
+import jwt from 'jsonwebtoken';
 
 import { app } from '../app';
 
 // declare global to make typescript aware of the global variable
 declare global {
-  var signin: () => Promise<string[]>;
+  var signin: () => string[];
 }
 
 let mongo: any;
@@ -38,28 +39,26 @@ afterAll(async () => {
 
 // this doesn't have to be defined here. (it's just an option)
 // It can be defined somewhere else and imported in test files.
-global.signin = async () => {
+global.signin = () => {
   // Build a JWT payload. { id, email}
+  const payload = {
+    id: '1lk24j124l',
+    email: 'test@test.com',
+  };
+
   // Create the JWT!
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
   // Build session Object. { jwt: MY_JWT }
+  const session = { jwt: token };
+
   // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+
   // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
   // return a string that's the cookie with the encoded data
+  // supertest takes this in array
+  return [`session=${base64}`];
 };
-
-// global.getCookieAfterSignup = async () => {
-//   const email = 'test@test.com';
-//   const password = 'password';
-
-//   const response = await request(app)
-//     .post('/api/users/signup')
-//     .send({
-//       email,
-//       password,
-//     })
-//     .expect(201);
-
-//   const cookie = response.get('Set-Cookie');
-
-//   return cookie;
-// };
