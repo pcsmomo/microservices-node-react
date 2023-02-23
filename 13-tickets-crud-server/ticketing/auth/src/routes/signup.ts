e1c1433b63b8438ad8a40d6da9075dd75e6a1be3
Noah Kim <dongwoo_333@hanmail.net>
2023-02-23 15:47:19 +1100

    chore: copy from the previous section

---
+++ 13-tickets-crud-server/ticketing/auth/src/routes/signup.ts
@@ -0,0 +1,101 @@
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+// common Middlewares and Errors
+import { validateRequest, BadRequestError } from '@dwktickets/common';
+
+// Models
+import { User } from '../models/user';
+
+const router = express.Router();
+
+/**
+ * @swagger
+ * tags:
+ *   name: User
+ *   description: User signup API
+ * /api/users/signup:
+ *   post:
+ *     summary: Signup a new user
+ *     tags: [User]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: The signed up user.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       500:
+ *         description: Some server error
+ *
+ */
+router.post(
+  '/api/users/signup',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be between 4 and 20 characters'),
+    validateRequest,
+  ],
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      throw new BadRequestError('Email in use');
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    // Store it on session object
+    // req.session.jwt = userJwt;  // This is not working in typescript
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(201).send(user);
+  }
+);
+
+export { router as signupRouter };
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - email
+ *         - password
+ *       properties:
+ *         email:
+ *           type: string
+ *           description: user email
+ *         password:
+ *           type: string
+ *           description: user password
+ *       example:
+ *         email: test@test.com
+ *         password: 12345678
+ */
