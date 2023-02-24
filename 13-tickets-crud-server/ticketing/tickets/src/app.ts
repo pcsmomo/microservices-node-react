e7fba16962a177ee369395a6e07336c562afb8c8
Noah Kim <dongwoo_333@hanmail.net>
2023-02-25 06:52:24 +1100

    288. Handling Updates

--- 13-tickets-crud-server/ticketing/tickets/src/app.ts
+++ 13-tickets-crud-server/ticketing/tickets/src/app.ts
@@ -6,6 +6,7 @@ import cookieSession from 'cookie-session';
 import { createTicketRouter } from './routes/new';
 import { showTicketRouter } from './routes/show';
 import { indexTicketRouter } from './routes';
+import { updateTicketRouter } from './routes/update';
 
 // common Middlewares and Errors
 import { errorHandler, NotFoundError, currentUser } from '@dwktickets/common';
@@ -24,6 +25,7 @@ app.use(currentUser); // this must be after use cookieSession
 app.use(createTicketRouter);
 app.use(showTicketRouter);
 app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all('*', async () => {
   throw new NotFoundError();
---
+++ 13-tickets-crud-server/ticketing/tickets/src/routes/update.ts
@@ -0,0 +1,27 @@
+import exporess, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import {
+  validateRequest,
+  NotFoundError,
+  requireAuth,
+  NotAuthorizedError,
+} from '@dwktickets/common';
+import { Ticket } from '../models/ticket';
+
+const router = exporess.Router();
+
+router.put(
+  '/api/tickets/:id',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const ticket = await Ticket.findById(req.params.id);
+
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    res.send(ticket);
+  }
+);
+
+export { router as updateTicketRouter };
