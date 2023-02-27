43cbf408e8d1009305042bb43b335775c81e088e
Noah Kim <dongwoo_333@hanmail.net>
2023-02-27 16:31:07 +1100

    329. Using the Custom Publisher

--- ticketing/nats-test/src/publisher.ts
+++ ticketing/nats-test/src/publisher.ts
@@ -1,5 +1,7 @@
 import nats from 'node-nats-streaming';
 
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
+
 console.clear();
 
 // client
@@ -10,13 +12,10 @@ const stan = nats.connect('ticketing', 'abc', {
 stan.on('connect', () => {
   console.log('Publisher connected to NATS');
 
-  const data = JSON.stringify({
+  const publisher = new TicketCreatedPublisher(stan);
+  publisher.publish({
     id: '123',
     title: 'concert',
     price: 20,
-  });
-
-  stan.publish('ticket:created', data, () => {
-    console.log('Event published');
   });
 });
