30055aec329027fa07402679caa3bfac29df4910
Noah Kim <dongwoo_333@hanmail.net>
2023-02-28 21:06:10 +1100

    test: ignore console.log in test console

--- ticketing/tickets/src/test/setup.ts
+++ ticketing/tickets/src/test/setup.ts
@@ -7,6 +7,16 @@ declare global {
   var signin: () => string[];
 }
 
+global.console = {
+  ...console,
+  // uncomment to ignore a specific log level
+  log: jest.fn(),
+  // debug: jest.fn(),
+  // info: jest.fn(),
+  // warn: jest.fn(),
+  // error: jest.fn(),
+};
+
 // __mocks__ folder is a special folder for jest
 // and it will automatically look for a file with the same name
 // as the module that we are trying to mock out.
