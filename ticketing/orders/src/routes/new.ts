0df9db61d7f8e7a59139621957172327ad290b98
Noah Kim <dongwoo_333@hanmail.net>
2023-03-01 16:00:58 +1100

    368. Finding Reserved Tickets

--- ticketing/orders/src/routes/new.ts
+++ ticketing/orders/src/routes/new.ts
@@ -4,6 +4,8 @@ import {
   requireAuth,
   validateRequest,
   NotFoundError,
+  BadRequestError,
+  OrderStatus,
 } from '@dwktickets/common';
 import { body } from 'express-validator';
 
@@ -35,6 +37,22 @@ router.post(
     }
 
     // Make sure that the ticket is not already reserved
+    // Run query to look at all orders. Find an order where the ticket
+    // is the ticket we just found *and* the orders status is *not* cancelled.
+    // If we find an order from that means the ticket *is* reserved
+    const existingOrder = await Order.findOne({
+      ticket: ticket,
+      status: {
+        $in: [
+          OrderStatus.Created,
+          OrderStatus.AwaitingPayment,
+          OrderStatus.Complete,
+        ],
+      },
+    });
+    if (existingOrder) {
+      throw new BadRequestError('Ticket is already reserved');
+    }
 
     // Calculate an expiration date for this order
 
