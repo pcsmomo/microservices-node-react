f8dfe9f33a129178246ff1cafd5d1b6ce622b816
Noah Kim <dongwoo_333@hanmail.net>
2023-03-03 06:58:00 +1100

    395. A Few More Reminders

--- ticketing/orders/src/events/listeners/ticket-created-listener.ts
+++ ticketing/orders/src/events/listeners/ticket-created-listener.ts
@@ -4,9 +4,12 @@ import { Subjects, Listener, TicketCreatedEvent } from '@dwktickets/common';
 // Models
 import { Ticket } from '../../models/ticket';
 
+// Constants
+import { queueGroupName } from './queue-group-name';
+
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   readonly subject = Subjects.TicketCreated;
-  queueGroupName = 'orders-service';
+  queueGroupName = queueGroupName;
 
   onMessage(data: TicketCreatedEvent['data'], msg: Message) {}
 }
