d6aed5472d748d77c2760a7b426148e8e6ad8e59
Noah Kim <dongwoo_333@hanmail.net>
2023-03-02 18:14:13 +1100

    387. Publishing the Order Creation

--- ticketing/orders/src/routes/new.ts
+++ ticketing/orders/src/routes/new.ts
@@ -13,6 +13,10 @@ import { body } from 'express-validator';
 import { Order } from '../models/order';
 import { Ticket } from '../models/ticket';
 
+// Events
+import { OrderCreatedPublisher } from '../events/publishers/order-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
+
 // Constants
 const EXPIRATION_WINDOW_SECONDS = 15 * 60; // 15 minutes
 
@@ -58,6 +62,16 @@ router.post(
     await order.save();
 
     // Publish an event saying that an order was created
+    new OrderCreatedPublisher(natsWrapper.client).publish({
+      id: order.id,
+      status: order.status,
+      userId: order.userId,
+      expiresAt: order.expiresAt.toISOString(), // convert to string (utc time zone)
+      ticket: {
+        id: ticket.id,
+        price: ticket.price,
+      },
+    });
 
     res.status(201).send(order);
   }
