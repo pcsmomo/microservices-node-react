acd6ce7c565c1143918c871e698acae74657e43c
Noah Kim <dongwoo_333@hanmail.net>
2023-03-03 18:15:36 +1100

    411. Updating Tickets Event Definitions

--- ticketing/tickets/src/routes/update.ts
+++ ticketing/tickets/src/routes/update.ts
@@ -40,6 +40,7 @@ router.put(
     await ticket.save(); // and the changes applies to ticket object
     new TicketUpdatedPublisher(natsWrapper.client).publish({
       id: ticket.id,
+      version: ticket.version,
       title: ticket.title,
       price: ticket.price,
       userId: ticket.userId,
