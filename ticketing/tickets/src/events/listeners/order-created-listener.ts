862edafec8acac61f90e681fdea66335aca541d4
Noah Kim <dongwoo_333@hanmail.net>
2023-03-04 16:26:55 +1100

    432. Private vs Protected Properties

--- ticketing/tickets/src/events/listeners/order-created-listener.ts
+++ ticketing/tickets/src/events/listeners/order-created-listener.ts
@@ -2,6 +2,8 @@ import { Message } from 'node-nats-streaming';
 import { Listener, OrderCreatedEvent, Subjects } from '@dwktickets/common';
 import { queueGroupName } from './queue-group-name';
 import { Ticket } from '../../models/ticket';
+import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publisher';
+import { natsWrapper } from '../../nats-wrapper';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
@@ -20,6 +22,7 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
     // Save the ticket
     await ticket.save();
+    new TicketUpdatedPublisher(natsWrapper.client);
 
     // Ack the message
     msg.ack();
