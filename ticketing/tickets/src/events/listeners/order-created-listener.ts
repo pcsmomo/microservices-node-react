c3fba5cc349311ad0528c3400e3447b41e17b261
Noah Kim <dongwoo_333@hanmail.net>
2023-03-04 16:02:34 +1100

    428. Reserving a Ticket

--- ticketing/tickets/src/events/listeners/order-created-listener.ts
+++ ticketing/tickets/src/events/listeners/order-created-listener.ts
@@ -1,9 +1,27 @@
 import { Message } from 'node-nats-streaming';
 import { Listener, OrderCreatedEvent, Subjects } from '@dwktickets/common';
 import { queueGroupName } from './queue-group-name';
+import { Ticket } from '../../models/ticket';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
-  async onMesage(data: OrderCreatedEvent['data'], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    // Find the ticket that the order is reserving
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    // If no ticket, throw error
+    if (!ticket) {
+      throw new Error('Ticket not found');
+    }
+
+    // Mark the ticket as being reserved by setting its orderId property
+    ticket.set({ orderId: data.id });
+
+    // Save the ticket
+    await ticket.save();
+
+    // Ack the message
+    msg.ack();
+  }
 }
