668623882a5d11bdb4e711ff0a29f9a1be40dc61
Noah Kim <dongwoo_333@hanmail.net>
2023-03-05 07:22:51 +1100

    438. Rejecting Edits of Reserved Tickets

--- ticketing/tickets/src/routes/__test__/update.test.ts
+++ ticketing/tickets/src/routes/__test__/update.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import mongoose from 'mongoose';
 
 import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
 import { natsWrapper } from '../../nats-wrapper';
 
 it('returns a 404 if the provided id does not exist', async () => {
@@ -107,4 +108,26 @@ it('publishes an event', async () => {
     .expect(200);
 
   expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
+
+it('rejects updates if the ticket is reserved', async () => {
+  const cookie = global.signin();
+
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({ title: 'asldkfj', price: 20 });
+
+  const ticket = await Ticket.findById(response.body.id);
+  ticket!.set({ orderId: global.generateId() });
+  await ticket!.save();
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'new title',
+      price: 100,
+    })
+    .expect(400);
 });
