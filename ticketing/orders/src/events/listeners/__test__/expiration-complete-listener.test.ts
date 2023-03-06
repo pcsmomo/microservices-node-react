58c1dca3700533df68d43f3872aafb8a66aa1510
Noah Kim <dongwoo_333@hanmail.net>
2023-03-06 14:24:43 +1100

    456. Testing the Expiration Complete Listener

---
+++ ticketing/orders/src/events/listeners/__test__/expiration-complete-listener.test.ts
@@ -0,0 +1,35 @@
+import { Message } from 'node-nats-streaming';
+import { ExpirationCompleteListener } from '../expiration-complete-listener';
+import { OrderStatus, ExpirationCompleteEvent } from '@dwktickets/common';
+import { natsWrapper } from '../../../nats-wrapper';
+import { Order } from '../../../models/order';
+import { Ticket } from '../../../models/ticket';
+
+const setup = async () => {
+  const listener = new ExpirationCompleteListener(natsWrapper.client);
+
+  const ticket = Ticket.build({
+    id: global.generateId(),
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+  const order = Order.build({
+    status: OrderStatus.Created,
+    userId: 'alskdjf',
+    expiresAt: new Date(),
+    ticket,
+  });
+  await order.save();
+
+  const data: ExpirationCompleteEvent['data'] = {
+    orderId: order.id,
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, order, ticket, data, msg };
+};
