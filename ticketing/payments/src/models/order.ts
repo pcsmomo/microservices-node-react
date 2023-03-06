f37ae431601e16491298120d087377ff1d671593
Noah Kim <dongwoo_333@hanmail.net>
2023-03-06 16:56:23 +1100

    465. Update-If-Current

--- ticketing/payments/src/models/order.ts
+++ ticketing/payments/src/models/order.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 import { OrderStatus } from '@dwktickets/common';
 
 interface OrderAttrs {
@@ -46,6 +47,9 @@ const orderSchema = new mongoose.Schema(
     },
   }
 );
+
+orderSchema.set('versionKey', 'version');
+orderSchema.plugin(updateIfCurrentPlugin);
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
   return new Order({
