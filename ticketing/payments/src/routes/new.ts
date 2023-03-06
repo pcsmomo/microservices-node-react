2c45af52f8feaeaf9fe374659e0819d9d31d22a6
Noah Kim <dongwoo_333@hanmail.net>
2023-03-07 06:46:30 +1100

    473. Validating Order Payment

--- ticketing/payments/src/routes/new.ts
+++ ticketing/payments/src/routes/new.ts
@@ -3,8 +3,10 @@ import { body } from 'express-validator';
 import {
   requireAuth,
   validateRequest,
-  BadRequestError,
   NotFoundError,
+  NotAuthorizedError,
+  BadRequestError,
+  OrderStatus,
 } from '@dwktickets/common';
 import { Order } from '../models/order';
 
@@ -18,8 +20,22 @@ router.post(
     body('orderId').not().isEmpty().withMessage('Order ID is required'),
   ],
   validateRequest,
-  (req: Request, res: Response) => {
-    res.send({ success: true });
+  async (req: Request, res: Response) => {
+    const { token, orderId } = req.body;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    if (order.status === OrderStatus.Cancelled) {
+      throw new BadRequestError('Cannot pay for an cancelled order');
+    }
+
+    res.status(201).send({ success: true });
   }
 );
 
