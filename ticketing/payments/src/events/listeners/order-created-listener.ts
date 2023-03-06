d3a0771297bff5f58b17ff5f4ccb87f72715442d
Noah Kim <dongwoo_333@hanmail.net>
2023-03-06 17:00:00 +1100

    466. Replicating Orders

---
+++ ticketing/payments/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,22 @@
+import { Message } from 'node-nats-streaming';
+import { Listener, OrderCreatedEvent, Subjects } from '@dwktickets/common';
+import { queueGroupName } from './queue-group-name';
+import { Order } from '../../models/order';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    const order = Order.build({
+      id: data.id,
+      version: data.version,
+      userId: data.userId,
+      price: data.ticket.price,
+      status: data.status,
+    });
+    await order.save();
+
+    msg.ack();
+  }
+}
