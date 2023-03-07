845df3f7299e90f9c863e8ea0a3d09e6534c0e43
Noah Kim <dongwoo_333@hanmail.net>
2023-03-08 08:03:17 +1100

    487. More on Publishing

--- ticketing/payments/src/routes/new.ts
+++ ticketing/payments/src/routes/new.ts
@@ -14,6 +14,10 @@ import { stripe } from '../stripe';
 import { Order } from '../models/order';
 import { Payment } from '../models/payment';
 
+// Events
+import { PaymentCreatedPublisher } from '../events/publishers/payment-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
+
 const router = express.Router();
 
 router.post(
@@ -51,7 +55,14 @@ router.post(
     });
     await payment.save();
 
-    res.status(201).send({ success: true });
+    // Publish an event saying that a payment was created
+    new PaymentCreatedPublisher(natsWrapper.client).publish({
+      id: payment.id,
+      orderId: payment.orderId,
+      stripeId: payment.stripeId,
+    });
+
+    res.status(201).send({ id: payment.id });
   }
 );
 
