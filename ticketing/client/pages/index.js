15e8e21ca105f29fbaa53c898f60c62005015e3e
Noah Kim <dongwoo_333@hanmail.net>
2023-03-09 18:20:30 +1100

    498. Listing All Tickets

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,8 +1,26 @@
-const LandingPage = ({ currentUser }) => {
-  return currentUser ? (
-    <h1>You are signed in</h1>
-  ) : (
-    <h1>You are NOT signed in</h1>
+const LandingPage = ({ currentUser, tickets }) => {
+  const ticketList = tickets.map(ticket => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
   );
 };
 
@@ -13,12 +31,15 @@ const LandingPage = ({ currentUser }) => {
 
 // getServerSideProps can replace getIinitialProps as getIinitialProps is considered as a legacy method
 // But leave it for now to see the usecase of getIinitialProps
-LandingPage.getInitialProps = async context => {
+LandingPage.getInitialProps = async (context, client, currentUser) => {
   // console.log('LANDING PAGE');
   // const client = buildClient(context);
   // const { data } = await client.get('/api/users/currentuser');
   // return data;
-  return {};
+
+  const { data } = await client.get('/api/tickets');
+
+  return { tickets: data };
 };
 
 export default LandingPage;
